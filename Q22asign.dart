98958c2650222899f8d12109d3d9a5cc8ec9071e
shaheer adeel <shaheeradeel57@gmail.com>
2024-02-12 07:29:39 -0800

    update

--- Q22asign.dart
+++ Q22asign.dart
@@ -3,6 +3,9 @@ void main(){
 // and values as quantities, write Dart code to check if a product named "Apple"
 // exists in the cart. Print "Product found" if it exists, otherwise print "Product not
 // found".
+
+
+
  Map<String, int> Product = {
      "apple": 2,
     "Mango": 3,
